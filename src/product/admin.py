efdbe58d5b4d5dcd1d55b358b3868ab5c1707ebc
IsmailTitas1815 <ismail15-1815@diu.edu.bd>
2022-09-25 01:59:47 +0600

    pagination done , filtering pending

--- src/product/admin.py
+++ src/product/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description','sku','created_at','updated_at')
+    readonly_fields = ('created_at','updated_at')
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
+# admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(Variant)
 admin.site.register(ProductVariant)
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListCreateAPIView
 from django.db.models import Q
 from product.models import *
 from product.serializers import *
+from django_filters.views import FilterView
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
@@ -16,8 +17,48 @@ class CreateProductView(generic.TemplateView):
 
 class ProductListView(generic.ListView):
     template_name = 'products/list.html'
-    model = Product
-    # paginate_by = 5
+    # model = Product
+    paginate_by = 3
+    filterset_class = None
+    def get_queryset(self):
+
+        try:
+            title = self.request.GET.get('title') if self.request.GET.get('title')!='' else None 
+            variant = self.request.GET.get('variant') if self.request.GET.get('variant')!='' else None
+            price_from = self.request.GET.get('price_from') if self.request.GET.get('price_from')!='' else None
+            price_to = self.request.GET.get('price_to') if self.request.GET.get('price_to')!='' else None
+            date = self.request.GET.get('date') if self.request.GET.get('date')!='' else None
+
+
+            if title or variant or price_from or price_to or date:
+                # print("title", title,"variant", variant,"price_from", price_from,"price_to", price_to,"date", date)
+                # print('\n\n')
+                product = product = Product.objects.select_related('productvariantprice', 'productvariant').filter(title__contains=title).all().values()
+
+                # if title:
+                #     product = Product.objects.select_related('productvariantprice', 'productvariant').filter(title__contains=title).all()
+
+                # if date:
+                #     print(product,"\n\n")
+                #     product = product.objects.all()
+                #     print("product 2",product)
+            else:
+                product = Product.objects.all().values()
+            # product = Product.objects.all().values()
+            newList = []
+            for prod in product:
+                newProduct = dict(prod)
+                prod_variant = ProductVariant.objects.select_related('variant','product').filter(product = prod['id']).all()
+                prodVarPrice = ProductVariantPrice.objects.select_related('product_variant_one','product_variant_two','product_variant_three').filter( (Q(product_variant_one__in=prod_variant) & Q(product_variant_two__in=prod_variant) & Q(product_variant_three__in=prod_variant)) & Q(product = prod['id'])).all()
+                newProduct['variant'] = prod_variant
+                newProduct['variant_price'] = prodVarPrice
+                newList.append(newProduct)
+            return newList
+            # return self.filterset.qs.distinct()
+        except:
+            return []
+
+
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
         product = Product.objects.all().values()
@@ -30,5 +71,6 @@ class ProductListView(generic.ListView):
             newProduct['variant_price'] = prodVarPrice
             newList.append(newProduct)
         context['product'] = newList
+        context['all_variant'] = Variant.objects.all()
         # context['product'] = product
         return context
